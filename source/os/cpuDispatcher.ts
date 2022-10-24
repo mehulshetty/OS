af25d376f84b205486dedfdfa40ca99fd43f1e9a
Mehul Shetty <mehul@148-100-171-145.foxnet.marist.edu>
2022-10-24 18:44:58 -0400

    My CPU works again, what a miracle

--- source/os/cpuDispatcher.ts
+++ source/os/cpuDispatcher.ts
@@ -4,7 +4,14 @@
    This virtual memory manager "allocates" and "deallocates" storage in memory..
    ------------ */
 module TSOS {
-    export class CpuScheduler {
+    export class CpuDispatcher {
+        constructor(public dispatcher: boolean = true) {
+        }
 
+        public switchContext() {
+            readyQueue[0].saveContext(_CPU);
+            readyQueue.push(readyQueue.shift());
+            readyQueue[0].getContext(_CPU);
+        }
     }
 }
